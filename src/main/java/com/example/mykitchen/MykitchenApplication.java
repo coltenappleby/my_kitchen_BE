4f0c8bdcc659d3d8a53333a7d46a9b95ba12bfe4
Colten Appleby <colten.appleby@gmail.com>
2023-12-21 13:44:04 -0500

    initial commit

---
+++ src/main/java/com/example/mykitchen/MykitchenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mykitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MykitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MykitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mykitchen/MykitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mykitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MykitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
